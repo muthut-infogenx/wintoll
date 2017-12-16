db07da446c7515a588a6587e01114bd7afbe7265
muthut-infogenx <34379205+muthut-infogenx@users.noreply.github.com>
2017-12-16 15:20:47 +0530

    mlm

---
+++ about.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html lang="en" class=""><!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+	<title>wintoll</title>
+
+	<!-- Standard Favicon -->
+	
+	<!-- For iPhone 4 Retina display: -->
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images//apple-touch-icon-114x114-precomposed.png">
+	
+	<!-- For iPad: -->
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images//apple-touch-icon-72x72-precomposed.png">
+	
+	<!-- For iPhone: -->
+	<link rel="apple-touch-icon-precomposed" href="images//apple-touch-icon-57x57-precomposed.png">	
+	
+	<link rel="stylesheet" type="text/css" href="libraries/lightslider/lightslider.min.css" />
+	
+	<link rel="stylesheet" type="text/css" href="revolution/css/settings.css">
+ 
+	<!-- RS5.0 Layers and Navigation Styles -->
+	<link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+	<link rel="stylesheet" type="text/css" href="revolution/css/navigation.css">
+	
+	<!-- Custom - Theme CSS -->
+	<link rel="stylesheet" type="text/css" href="styles.css" />
+
+	<!--[if lt IE 9]>
+		<script src="js/html5/respond.min.js"></script>
+    <![endif]-->
+	
+</head>
+
+<body data-offset="200" data-spy="scroll" data-target=".ow-navigation">
+
+	<a id="top"></a>
+	
+		<!-- Header Section -->
+	<header id="header" class="header-section element-header container-fluid no-padding">		
+		<!-- nav -->
+		<nav class="navbar navbar-default ow-navigation">
+			<div class="navbar-header">
+				<button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="index.html" class="logoimg"><img src="images/logo.png"></a>
+			</div>
+			
+			<div class="navbar-collapse collapse navbar-right" id="navbar">
+				<ul class="nav navbar-nav">
+					<li class="active mega-dropdown">
+						<a href="index.php" title="Home" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Home</a>						
+					</li>
+					<li class="">
+						<a href="about.php" title="Pages" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">About</a>			
+					</li>
+					<li class="mega-dropdown">
+						<a href="opportunity.php" title="Portfolio" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Opportunity</a>						
+					</li>
+					 
+					<li class="">
+						<a href="gallery.php" title="Shop" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Gallery</a>						
+					</li>
+					<li class=" mega-dropdown">
+						<a href="login.php" title="Elements" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Sign In</a>						
+					</li>
+					<li class="">
+						<a href="register.php" title="Shop" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Sign Up</a>						
+					</li>
+					<li class="">
+						<a href="contact.php" title="Shop" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Contact</a>						
+					</li>
+					 
+				</ul>
+			</div><!--/.nav-collapse -->
+		</nav><!-- nav /- -->
+		<!-- Search Box -->
+		<div class="search-box">
+			<span><i class="icon_close"></i></span>
+			<form>
+				<input type="text" class="form-control" placeholder="Enter a keyword and press enter..." />
+			</form>
+		</div><!-- Search Box /- -->
+	</header><!-- Header Section /- -->
+	
+	<main class="site-main page-spacing">
+	<!-- Page Breadcrumb -->
+		<div class="page-breadcrumb container-fluid padding-10">
+			<div class="container">
+				<ol class="breadcrumb">
+					<li><a href="#">Home</a></li>
+					<li class="active">About</li>
+				</ol>
+			</div>
+		</div>
+	
+	<div class="content-slide-section">
+		<div class="container">
+			<div>
+				<h1>Bank Deatils</h1>
+				<p>Shoppe TRADEMART LLP</p>
+			<table class="table" border="1">
+			<tr>
+				<th align="bottom">Bank Name</th>
+				<th>CANARA BANK</th>
+				<th rowspan="5" align="center"><img src="images/bank.jpg" alt="content-image-2"></th>
+			</tr>
+			<tr>
+				<th nowrap>Account Name</th>
+				<th nowrap>SHOPPE TRADEMART LLP</th>
+			</tr>
+			<tr>
+				<th>Account No.</th>
+				<th>5575201000147</th>
+			</tr>
+			<tr>
+				<th>Branch</td>
+				<th>SIKAR INDUSTRIAL AREA</th>
+			</tr>
+			<tr>
+				<th>IFSC Code</th>
+				<th>CNRB0005575</th>
+			</tr>
+
+		</table>
+	</div>
+	</div>
+	</div>
+	<div class="content-slide-section content-slide-bg container-fluid no-padding">
+			<div class="container">
+				<div class="col-md-6 col-sm-6 col-xs-6 content-block">
+								<h2>Vision & Mission</h2>
+			<span>We live in a world where the young reaching higher, dreaming bigger and demanding more; a world that is challenging the limits of hope and possibility. Nowhere is more strikingly visible than in India – a country that wakes up every morning a little younger in age, but infinitely more ambitious in spirit. </span>
+			<span>Our goal is not just to build a great enterprise in for our stakeholders, but, more importantly, to build a great future for our country and the world at large; to give hundreds of millions of people the power to fulfill their dreams, shape their own destiny, and the means to realize their true and diverse potential. </span>
+			<span>As One Family, we live by the philosophy of vasudhavia kutumbakam, which reflects in the ethos of the company. Our Endeavour has been to unify the entire world with our rich and engaging content, leading to the upliftment of society and we are constantly working towards bringing in a posting change in the overall cultural milieu. At Shoppetrade, the Financial year 2017-18 has been and will be a year of'' Investment for Tomorrow"</span>
+			<span>Jai Hind</span>
+			<a href="#">read more</a>
+							</div>
+							<div class="col-md-6 col-sm-6 col-xs-6 content-image-block">
+					<img src="images/mission.jpg" alt="content-image-2">
+				</div>
+						</div>
+					</div>
+
+				</main>
+
+	<!-- Footer 6 -->
+	<div class="footer-main footer-section2 footer6 container-fluid no-padding">
+		<div class="padding-60"></div>
+		<!-- Container -->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+					<aside class="ftr-widget ftr_about_widget">
+						<a href="index.html" class=""><img src="images/logo.png"></a>
+						<p>We live in a world where the young reaching higher, dreaming bigger and demanding more; a world that is challenging the limits of hope and possibility. Nowhere is more strikingly visible than in India – a country that wakes up every morning a little younger in age, but infinitely more ambitious in spirit.</p>
+						<p><i class="fa fa-map-marker"></i>W105NN Portobello Square, London</p>
+						<p><i class="fa fa-phone"></i> <a href="tel:+555987654123" title="+555987654123">+000 000 000 000</a></p>
+						<p><i class="fa fa-envelope-o"></i> <a href="mailto:info@metrica.com" title="info@metrica.com">info@wintoll.com</a></p>
+						<ul>
+							<li><a href="#" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#" title="Twiiter"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#" title="Google Plus"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</aside>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<aside class="ftr-widget ftr_link">
+						<h3 class="widget-title">Esencia Global Marketing Pvt. Ltd.</h3>
+						<ul>
+							<li><a href="about.php">About us</a></li>
+							<li><a href="about.php">Vision & Mission</a></li>
+							<li><a href="about.php">Bank Details</a></li>
+							<li><a href="term.php">Terms</a></li>
+							<li><a href="gallery.php">Gallery</a></li>
+						</ul>
+					</aside>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<aside class="ftr-widget ftr_link">
+						<h3 class="widget-title">NAVIGATION</h3>
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="joining.php">Joining Options</a></li>
+							<li><a href="opportunity.php">Opportunity</a></li>
+							<li><a href="login.php">Sign in</a></li>
+							<li><a href="register.php">Sign up</a></li>
+							<li><a href="contact.php">Contact us</a></li>
+					</aside>
+				</div>
+							
+			</div>
+		</div><!-- Container /- -->
+		<div class="copyright container-fluid">
+			<div class="container">
+				<p>&copy; Copyright 2017 wintoll. All rights reserved.</p>
+			</div>
+		</div><!-- Bottom Footer /- -->
+	</div><!-- Footer 6 /- -->
+	
+	
+	<!-- JQuery v1.11.3 -->
+	<script src="js/jquery.min.js"></script>
+	
+	<!-- Library JS -->
+	<script src="libraries/lib.js"></script>
+	<script src="libraries/lightslider/lightslider.min.js"></script>
+	
+	<!-- RS5.0 Core JS Files -->
+	<script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js?rev=5.0"></script>
+	<script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+	<!-- Library - Google Map API -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDW40y4kdsjsz714OVTvrw7woVCpD8EbLE"></script>
+	
+	<!-- Library - Theme JS -->	
+	<script src="js/Chart.min.js"></script>	
+	<script src="js/jquery.knob.js"></script>
+	<script src="js/functions.js"></script>
+
+</body>
+</html>
